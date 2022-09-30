76eaec8d826376d85553160e3d4b27db5043a6f0
silvotape <sperelli@outlook.com>
2022-09-29 23:10:28 -0300

    Desafio Eventos

--- script.js
+++ script.js
@@ -1,6 +1,77 @@
 // SIMULADOR DE RESERVAS
 
-let tarifa = prompt ("Que tarifa desea conocer? Escriba: matutina, after, hora o ESC para salir");
+let matutina = document.getElementById("matutina");
+matutina.addEventListener("click",tarifaMatutina);
+
+let after = document.getElementById("after");
+after.addEventListener("click",tarifaAfter);
+
+let hora = document.getElementById("hora");
+hora.addEventListener("click",tarifaHora);
+
+function tarifaMatutina(){
+  contenedor.innerHTML = ""
+  let parrafo= document.createElement("p");
+  parrafo.innerHTML="<p>El horario para la tarifa Matutina es 08:00 a 12:00 y su costo es $2500</p>" 
+  let matutinaResp = document.getElementById("contenedor");
+  matutinaResp.append(parrafo); 
+  }
+
+  function tarifaAfter(){
+  contenedor.innerHTML = ""
+  let parrafo= document.createElement("p");
+  parrafo.innerHTML="<p>El horario para la tarifa de After es 19:00 a 21:00 incluye 1 pizzeta y 2 tragos y su costo es 3500</p>" 
+  let afterResp = document.getElementById("contenedor");
+  afterResp.append(parrafo);  
+  }
+
+function tarifaHora(){
+  contenedor.innerHTML = ""
+  let parrafo= document.createElement("p");
+  parrafo.innerHTML="<p>El costo de la hora es de $740</p>" 
+  let horaResp = document.getElementById("contenedor");
+  horaResp.append(parrafo); 
+  }
+
+
+  let formulario = document.getElementById("formulario");
+  console.log(formulario)
+  formulario.addEventListener("submit",(e) => {
+    e.preventDefault();
+    let inputs = e.target.children;
+
+    console.log(inputs[1].value);
+    console.log(inputs[3].value); 
+    console.log(inputs[5].value);
+
+  } 
+  )
+
+  class Persona {
+    constructor (nombre, contacto, email){
+      this.inputs[1].value = nombre,
+      this.inputs[3].value = contacto,
+      this.inputs[5].value = email
+    }  
+  }
+
+  const persona1 = new Persona (nombre, contacto, email);
+  let reservaRealizada = `Ha quedado efectiva la reserva a nombre de:\n ${persona1.nombre}\n ${persona1.contacto}\n ${persona1.email}\n`;
+  alert (reservaRealizada);
+  console.log(persona1);
+
+//Se agrega la persona que gestiono la reserva a un array.
+
+const reservas = [];
+reservas.push(reserva.persona1);
+console.log(reservas);
+
+
+
+/* const reservas = [];
+reservas.push(reserva.persona1);
+console.log(reservas); */
+/* let tarifa = prompt ("Que tarifa desea conocer? Escriba: matutina, after, hora o ESC para salir");
 
 while (tarifa != "ESC") {
   switch (tarifa) {
@@ -104,3 +175,4 @@ function tarifaHora(){
   };
 }; 
 
+ */
